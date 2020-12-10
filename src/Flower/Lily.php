a4be2da30d3fc8ba323fe349986562de28f48e5e
liuwang <616458279@qq.com>
2020-12-10 11:17:10 +0800

    Initial commit

---
+++ src/Flower/Lily.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lingan
+ * Date: 2017/11/6
+ * Time: 21:39
+ */
+
+namespace Flower\Lily;
+
+class Lily
+{
+    public function desc()
+    {
+        echo "this is Lily flower";
+    }
+}
